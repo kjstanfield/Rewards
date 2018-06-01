979b070b824b34200c3dceaf0e7c75ecadd3e04d
Kyle Stanfield <kjstan@gmail.com>
2018-05-31 22:32:57 -0400

    Added node and express.

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+// src/config/index.js
+
+module.exports = {
+  appName: 'KFC HD Rewards',
+  port: 3030
+}
---
+++ src/server.js
@@ -0,0 +1,18 @@
+// src/server.js
+
+const path = require('path')
+const express = require('express')
+const config = require('./config')
+
+const app = express()
+
+const publicPath = path.resolve(__dirname, '../public')
+app.use(express.static(publicPath))
+
+app.use(function (req, res, next) {
+  res.end('Hello World!')
+})
+
+app.listen(config.port, function () {
+  console.log(`${config.appName} is listening on port ${config.port}`)
+})
