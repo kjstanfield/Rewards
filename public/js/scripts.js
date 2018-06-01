5d3b52f5179c005ff3bb29c0b228f126a3f25c5b
Kyle Stanfield <kjstan@gmail.com>
2018-06-01 12:24:31 -0400

    Corrected syntax error, fake data now loads correctly.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,7 @@
 // Attempt to add main list elements
 
 $.get('/api/emp', function (employees) {
-  for (let emp in employees) {
+  for (let emp of employees) {
     $('#mainlist ul').append(`                   
     <li class="list-group-item d-flex justify-content-between align-items-right">
     <div>${emp.name}</div>
@@ -9,5 +9,19 @@ $.get('/api/emp', function (employees) {
       ${emp.coins} <i class="far fa-coins"></i> 
     </span>
     </li>`)
+  }
+})
+
+$.get('/api/emp', function (employees) {
+  for (let emp of employees) {
+    $('#adminlist ul').append(`                   
+    <li class="list-group-item d-flex justify-content-between align-items-right">
+    <div>${emp.name}</div>
+    <span class="controls">
+        <i class="far fa-coins"></i> ${emp.coins} 
+        <i class="far fa-pencil"></i>
+        <i class="fal fa-trash-alt"></i>
+    </span>
+</li>`)
   }
 })
